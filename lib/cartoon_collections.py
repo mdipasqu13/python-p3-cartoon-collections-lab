8f2d173b3a0e95d76caaa210d3333a648bb67602
Michael DiPasquale <michaeldipasquale313@gmail.com>
2024-04-02 11:00:35 -0400

    completed lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,19 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for i, dwarf in enumerate(dwarves, 1):
+        print(f"{i}. {dwarf}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    return [call.capitalize() + '!' for call in planeteer_calls]
+    
+def long_planeteer_calls(list_of_calls):
+    for call in list_of_calls:
+        if len(call) > 4:
+            return True
+    return False
 
-def long_planeteer_calls():
-    pass
-
-def find_the_cheese():
-    pass
+def find_the_cheese(list_of_snacks):
+    list_of_cheese = ["cheddar", "gouda", "camembert"]
+    for item in list_of_snacks:
+        if item in list_of_cheese:
+            return item
+    return None
